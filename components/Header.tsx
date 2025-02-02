a438b3c01c3660beea5861f34f04f6a51060c437
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2025-02-02 14:07:51 +0100

    cn

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,26 @@
+"use client"
+
+import { usePathname } from 'next/navigation'
 import React from 'react'
+import Link from 'next/link'
+import { cn } from '@/lib/utils'
 
 const Header = () => {
+    const pathname = usePathname()
+
+
   return (
-    <div>Header</div>
+    <header className='flex my-10 justify-between gap-5'>
+        <Link href="/">BookWise</Link>
+        
+        <ul className="flex flex-row items items-center gap-8">
+            <li>
+                <Link href="/library" className={cn("text-base cursor-pointer capitalize", pathname === "/library" ? "text-light-200" : "text-light-100",)}>
+                     Library
+                </Link>
+            </li>
+        </ul>
+    </header>
   )
 }
 
